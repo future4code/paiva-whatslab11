6a2a65464935db4eda9c48216737fa8d1d66cb86
dan-ueno <ueno2005@gmail.com>
2021-05-09 00:24:17 -0300

    styled.js criado e falta implementação de enter para envio de mensagem

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,96 +1,10 @@
-import logo from './logo.svg';
-import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 import img from "./img/enviar.png"
+import {Container, InputsDiv, InputTexts ,Button, ImgButton, Mensagens, BalaoDeMensagem} from "./styled"
 
-const Container = styled.div`
-  display: flex;
-  border: 1px solid;
-  width: 500px;
-  height: 100vh;
-  flex-direction: column-reverse;
-  margin: 0 auto;
-  padding: 0;
-  background-color: #e7ddd7;
-  box-sizing: border-box;
-`;
- const InputsDiv = styled.div`
-  margin: 5px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  box-sizing: border-box;
-  height: 5%;
-`;
-const InputTexts = styled.input`
-  border: none;
-  border-radius: 0.5em;
-  margin: 0px;
-  padding: 5px;
-  align-content: center;
-  width:${props => {
-        if (props.tipo === "nome") {
-            return "15%"
-        } else {
-            return "60%"
-        }
-    }};
-`
-const Button = styled.button `
-border-radius: 0.5em;
-width: 15%;
-border: none;
-`
-const Mensagens = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: ${props => {
-        if (props.tipo === "eu") {
-            return "right"
-        } else {
-            return "left"
-        }
-    }};
-`
-const ImgButton = styled.img`
-width: 100%;
-`
- const BalaoDeMensagem = styled.div`
-  margin: 0.5em;
-  max-width: 60%;
-  padding: 0.9em 0.8em;
-  border-radius: 0.5em;
-  font-weight: 450;
-  line-height: 1.3;
-  word-wrap: break-word;
-  max-width: 60%;
-  min-width: 8%;
-  margin-bottom: 1em;
-  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
-  
-  background-color: ${props => {
-        if (props.tipo === "eu") {
-            return "#DDF7C8" // Verde copiado do WhatsApp
-        } else {
-            return "#ffffff" // Branco
-        }
-    }};
- 
- align-self:  ${props => {
-        if (props.tipo === "eu") {
-            return "flex-end"
-        } else {
-            return "flex-start"
-        }
-    }};
-
-`;
-
-//acaba o styled
-class App extends React.Component {
+class App extends React.Component{
   state = {
-    mensagens: [{id:1, nome:"Daniel", mensagem: "fica na esquerda"},{id:2, nome:"eu", mensagem: "fica pra direita"}
-    ],
+    mensagens: [{id:1, nome:"Daniel", mensagem: "Estou na esquerda!"},{id:Date.now(), nome:"eu", mensagem: "Estou na direita!"}],
     inputName:"",
     inputMsg: "",
   };
@@ -98,39 +12,48 @@ class App extends React.Component {
     this.setState({ inputName: event.target.value });
   }
   handleMsg = (event) => {
-    this.setState({ inputMsg: event.target.value });
+      this.setState({ inputMsg: event.target.value });
   }
   onClickEnviar = () =>{
-    const novaMsg = {id: Math.random(), nome:this.state.inputName, mensagem: this.state.inputMsg}    
+    const novaMsg = {id: Date.now(), nome:this.state.inputName, mensagem: this.state.inputMsg}    
     this.setState({
       mensagens: [...this.state.mensagens,novaMsg],
       inputName:"",
       inputMsg: "",
     })
-    
   }
-  render () {
-const chat = this.state.mensagens.map((msg)=> {
-  if (msg.nome === "eu") {
-            return (
-              <Mensagens tipo={"eu"}>
-                <BalaoDeMensagem tipo={"eu"}>
-                    {msg.mensagem}   
-                </BalaoDeMensagem>
-                </Mensagens>
-            )
-        } else {
-            return (
-              <Mensagens tipo={"outro"}>
-                <BalaoDeMensagem tipo={"outro"}>
-                    <div><b>{msg.nome}</b></div>
-                    <div>{msg.mensagem}</div>
-                </BalaoDeMensagem>
-                </Mensagens>
-            )
-        }
-})
-
+doubleClickApagar = (idPost) =>{
+  const atualizaMsg = this.state.mensagens.filter((post) =>{
+    if (post.id !==idPost){
+      return true;
+    }
+  })
+  this.setState({
+    mensagens: [...atualizaMsg]
+  })
+}
+render () {
+  const chat = this.state.mensagens.map((msg)=> {
+    if (msg.nome === "eu") {
+              return (
+                <Mensagens tipo={"eu"} key={msg.id} onDoubleClick={()=>{this.doubleClickApagar(msg.id)}}>
+                  <BalaoDeMensagem tipo={"eu"}>
+                      {msg.mensagem}   
+                  </BalaoDeMensagem>
+                  </Mensagens>
+              )
+          } else {
+              return (
+                <Mensagens tipo={"outro"} key={msg.id} onDoubleClick={()=>{this.doubleClickApagar(msg.id)}}>
+                  <BalaoDeMensagem tipo={"outro"}>
+                      <div><b>{msg.nome}</b></div>
+                      <div>{msg.mensagem}</div>
+                  </BalaoDeMensagem>
+                  </Mensagens>
+              )
+          }
+  })
+  
   return (
     <Container>
       <InputsDiv>
@@ -143,7 +66,7 @@ const chat = this.state.mensagens.map((msg)=> {
           placeholder={"Mensagem"}
           value={this.state.inputMsg}
           onChange={this.handleMsg}
-
+          
         />
         <Button onClick={this.onClickEnviar}><ImgButton src={img}/></Button>
       </InputsDiv>
---
+++ whatslab/src/styled.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  border: 1px solid;
+  max-width: 500px;
+  height: 100vh;
+  flex-direction: column-reverse;
+  margin: 0 auto;
+  padding: 0;
+  background-color: #e7ddd7;
+  box-sizing: border-box;
+`;
+ export const InputsDiv = styled.div`
+  margin: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  box-sizing: border-box;
+  height: 5%;
+`;
+export const InputTexts = styled.input`
+  border: none;
+  border-radius: 0.5em;
+  margin: 0px;
+  padding: 5px;
+  align-content: center;
+  width:${props => {
+        if (props.tipo === "nome") {
+            return "15%"
+        } else {
+            return "60%"
+        }
+    }};
+`
+export const Button = styled.button `
+border-radius: 0.5em;
+width: 15%;
+border: none;
+`
+export const ImgButton = styled.img`
+width: 100%;
+`
+
+export const Mensagens = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: ${props => {
+        if (props.tipo === "eu") {
+            return "right"
+        } else {
+            return "left"
+        }
+    }};
+`
+ export const BalaoDeMensagem = styled.div`
+  margin: 0.5em;
+  max-width: 60%;
+  padding: 0.9em 0.8em;
+  border-radius: 0.5em;
+  font-weight: 450;
+  line-height: 1.3;
+  word-wrap: break-word;
+  max-width: 60%;
+  min-width: 8%;
+  margin-bottom: 1em;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+  
+  background-color: ${props => {
+        if (props.tipo === "eu") {
+            return "#DDF7C8" // Verde copiado do WhatsApp
+        } else {
+            return "#ffffff" // Branco
+        }
+    }};
+ 
+ align-self:  ${props => {
+        if (props.tipo === "eu") {
+            return "flex-end"
+        } else {
+            return "flex-start"
+        }
+    }};
+
+`;
